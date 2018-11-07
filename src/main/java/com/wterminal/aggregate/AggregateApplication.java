2dad683ecdcc389d4f2e37515b726ba1818b9c4d
chenjian07 <chenjian07@shengpay.com>
2018-11-07 17:11:49 +0800

    first commit

---
+++ src/main/java/com/wterminal/aggregate/AggregateApplication.java
@@ -0,0 +1,12 @@
+package com.wterminal.aggregate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AggregateApplication.class, args);
+    }
+}
